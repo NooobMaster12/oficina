ded0b8f91fbbe31ce14df05137c290959645c5fc
NooobMaster12 <isaqueabraaodesousa0000@gmail.com>
2024-03-18 21:30:12 -0300

    login

--- oficina/urls.py
+++ oficina/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.usuarios import views
 
 urlpatterns = [
+    path('login/', views._login, name='login'),
     path('admin/', admin.site.urls),
 ]
