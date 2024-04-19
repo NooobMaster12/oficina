988cca2214decf236981327e4a4f53fb2f9dc3f8
NooobMaster12 <isaqueabraaodesousa0000@gmail.com>
2024-04-18 21:25:56 -0300

    mecanicos

--- apps/geral/urls.py
+++ apps/geral/urls.py
@@ -3,12 +3,15 @@ from . import views
 
 app_name = 'geral'
 
-
 urlpatterns = [
-    path('nova-oficina/', views.nova_oficina, name='nova_oficina'),
+    path('mecanicos', views.lista_mecanico, name='lista_mecanicos'),
+    path('novo_mecanico/', views.novo_mecanico, name='novo_mecanico'),
+    path('excluir_mecanico/<int:pk>/', views.apagar_mecanico, name='apagar_mecanico'),
+    path('editar_mecanico/<int:pk>/', views.editar_mecanico, name='editar_mecanico'),
+    path('nova-oficina', views.nova_oficina, name='nova_oficina'),
     path('oficinas/', views.lista_oficina, name='lista_oficina'),
+    path('excluir_oficina/<int:pk>/', views.apagar_oficina, name='apagar_oficina'),
+    path('editar_oficina/<int:pk>/', views.editar_oficina, name='editar_oficina'),
     path('', views.home, name='home'),
-    path('exlcuir-oficina/<int:pk>/', views.excluir_oficina, name='excluir_oficina'),
-    path('editar-oficina/<int:pk>/', views.editar_oficina, name='editar_oficina')
-    # path('exlcuir_oficina/<int:id>/', views.apagar_oficina, name='apagar_oficina')
+    
 ]
--- apps/geral/views.py
+++ apps/geral/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
+from .forms import OficinaForm, MecanicoOficina
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Oficina, Mecanico
 
-from .forms import OficinaForm
-from .models import Oficina
-# Create your views here.
-
-# @login_required
+@login_required
 def home(request):
     template_name = 'geral/home.html'
     context = {}
     return render(request, template_name, context)
+
 
 
 @login_required
@@ -20,16 +19,22 @@ def nova_oficina(request):
     if request.method == 'POST':
         form = OficinaForm(request.POST)
         if form.is_valid():
-           of = form.save(commit=False)
-           of.usuario = request.user
-           of.save()
-           messages.success(request, 'Oficina cadastrada com suscesso.')
-           return redirect('geral:lista_oficina')
-    form = OficinaForm()
-    context['form'] = form
+            of = form.save(commit=False)
+            of.usuario = request.user
+            of.save()
+            messages.success(request, 'Oficina cadastrada com sucesso!')
+            return redirect('geral:lista_oficina')
+        else:
+            messagem_erro = list(form.erros.values())[0][0]
+            messages.error(request, f'{messagem_erro}')
+            return redirect('geral:lista_oficina')
+        
+    form = OficinaForm()    
+    context['form'] = form    
     return render(request, template_name, context)
 
 
+@login_required
 def lista_oficina(request):
     template_name = 'geral/lista_oficina.html'
     oficinas = Oficina.objects.filter(usuario=request.user)
@@ -40,24 +45,26 @@ def lista_oficina(request):
 
 
 
-@login_required
-def excluir_oficina(request, pk):
-    oficina = get_object_or_404(Oficina, pk=pk)
-    oficina.delete()
-    messages.info(request, 'Oficina excluida com sucesso')
-    return redirect('geral:lista_oficina')
-
 # @login_required
 # def apagar_oficina(request, id):
-#     oficina = Oficina.objects.get(id=id)
+#     oficina = Oficina.objects.get(usuario=request.user, id=id)    
 #     oficina.delete()
 #     return redirect('geral:lista_oficina')
+
+@login_required
+def apagar_oficina(request, pk):
+    oficina = get_object_or_404(Oficina, pk=pk)
+    oficina.delete()
+    messages.info(request, 'Oficina excluída com sucesso!')
+    return redirect('geral:lista_oficina')
 
 
 @login_required
 def editar_oficina(request, pk):
     template_name = 'geral/nova_oficina.html'
-    context = {}
+    context = {
+
+    }
     oficina = get_object_or_404(Oficina, pk=pk)
     if request.method == 'POST':
         form = OficinaForm(data=request.POST, instance=oficina)
@@ -65,5 +72,71 @@ def editar_oficina(request, pk):
         messages.success(request, 'Oficina atualizada com sucesso!')
         return redirect('geral:lista_oficina')
     form = OficinaForm(instance=oficina)
-    context['form'] = form
     return render(request, template_name, context)
+    
+
+
+# @login_required
+# def atualizar_oficina(request, pk):
+#     template_name = 'geral/nova_oficina.html'
+#     oficina = Oficina.objects.get(usuario=request.user, id=id)
+#     forms = OficinaForm(instance=oficina)
+#     context = {
+#         'forms': forms,
+#     }
+#     if request.method == "POST":
+#     return render(request, template_name, context)
+
+@login_required
+def novo_mecanico(request):
+    template_name = 'geral/novo_mecanico.html'
+    context = {}
+    if request.method == 'POST':
+        form = MecanicoOficina(request.POST)
+        usuario = request.user
+        oficina = get_object_or_404(Oficina, usuario=usuario)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.oficina = oficina
+            form.save()
+            messages.success(request, "Mecânico adicionado com sucesso!")
+            return redirect('geral:lista_mecanicos')
+    form = MecanicoOficina()
+    context['form'] = form   
+    return render(request, template_name, context)
+
+
+@login_required
+def lista_mecanico(request):
+    template_name = 'geral/lista_mecanicos.html'
+    usuario = request.user
+    oficina = Oficina.objects.get(usuario=usuario)
+    mecanicos = Mecanico.objects.filter(oficina=oficina)
+    context = {
+        'mecanicos': mecanicos,
+    }
+    return render(request, template_name, context)
+
+
+@login_required
+def editar_mecanico(request, pk):
+    template_name = 'servicos/novo_mecanico.html'
+    context = {
+
+    }
+    mecanico = get_object_or_404(Mecanico, pk=pk)
+    if request.method == 'POST':
+        form = OficinaForm(data=request.POST, instance=mecanico)
+        form.save()
+        messages.success(request, 'Mecânico atualizado com sucesso!')
+        return redirect('geral:lista_mecanicos')
+    form = OficinaForm(instance=mecanico)
+    return render(request, template_name, context)
+
+
+@login_required
+def apagar_mecanico(request, pk):
+    mecanico = get_object_or_404(Mecanico, pk=pk)
+    mecanico.delete()
+    messages.info(request, 'Mecânico excluído com sucesso!')
+    return redirect('geral:lista_mecanicos')
